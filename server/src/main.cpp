f11ee2a05e8751dcba373dbbfbacd7cf192616b2
Flavian Gontier <flavian.gontier@gmail.com>
2018-09-26 16:38:32 +0200

    Server updated

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -2,3 +2,12 @@
 // Created by flaviangontier on 9/26/18.
 //
 
+#include <core/net/TcpServer.hpp>
+
+int main(int argc, char **argv)
+{
+    marguerite::net::TcpServer server("127.0.0.1", 6666);
+
+    server.start();
+    return (0);
+}
